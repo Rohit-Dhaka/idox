eafc49af22b36c9dda149494e94f193deacb81d3
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-04-02 10:14:10 +0530

    6th section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,68 +1,113 @@
-    // --------nav-bar
-    const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector(".menulist");
-    hamburger.addEventListener("click", mobileMenu);
-    const Navlink  = document.querySelectorAll(".nav-link");
-    Navlink.forEach( e  => e.addEventListener("click" , ()=>{
-        hamburger.classList.toggle("nav-active");
-        document.body.classList.remove("overflow-hidden");
-        navMenu.classList.toggle("nav-active");
-    }))
-    function mobileMenu() {
-      hamburger.classList.toggle("nav-active");
-      document.body.classList.toggle("overflow-hidden");
-      navMenu.classList.toggle("nav-active");
-    }
+// --------nav-bar
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menulist");
+hamburger.addEventListener("click", mobileMenu);
+const Navlink = document.querySelectorAll(".nav-link");
+Navlink.forEach(e => e.addEventListener("click", () => {
+  hamburger.classList.toggle("nav-active");
+  document.body.classList.remove("overflow-hidden");
+  navMenu.classList.toggle("nav-active");
+}))
+function mobileMenu() {
+  hamburger.classList.toggle("nav-active");
+  document.body.classList.toggle("overflow-hidden");
+  navMenu.classList.toggle("nav-active");
+}
 
 
 
-    
 
 
 
-     // --------slider
-     $('.first-slider').slick({      
-        prevArrow: ".pre-btn",
-        nextArrow: ".next-btn",
+
+// --------slider
+$('.first-slider').slick({
+  prevArrow: ".pre-btn",
+  nextArrow: ".next-btn",
+  infinite: true,
+  dots: false,
+  speed: 300,
+  slidesToShow: 8,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 3000,
+  pauseOnHover: false,
+  cssEase: 'linear',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
         infinite: true,
-        dots: false,
-        speed: 300,
-        slidesToShow: 8,
-        slidesToScroll: 1,                
         autoplay: true,
-        autoplaySpeed: 0,
-        speed: 8000,
-        pauseOnHover: false,
-        cssEase: 'linear',
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true, 
-              autoplay: true,           
-              cssEase: 'linear'
-              
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              autoplay: true,     
-              cssEase: 'linear'      
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              autoplay: true,      
-              cssEase: 'linear'     
-            }
-          }                        
-        ]
-      });
+        cssEase: 'linear'
+
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        cssEase: 'linear'
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        cssEase: 'linear'
+      }
+    }
+  ]
+});
+
+
+// --------slider-two
+$('.slider-two').slick({    
+  infinite: true,
+  dots: true,
+  speed: 300,
+  slidesToShow: 2,
+  centerMode: true,
+  dotsClass: 'slick-dots',
+  slidesToScroll: 1,
+  // autoplay: true,      
+  cssEase: 'linear',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        cssEase: 'linear'
+
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        cssEase: 'linear'
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        cssEase: 'linear'
+      }
+    }
+  ]
+});
